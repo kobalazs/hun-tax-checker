12fab7d8e2dacb7ee0d16094b9ead61c8edb3c09
Balázs Kovács <kobalazs@gmail.com>
2019-03-13 20:42:13 +0100

    Implement: EVNY test

--- tests/evny_test.js
+++ tests/evny_test.js
@@ -1,7 +1,17 @@
 
 Feature('Egyéni vállalkozók nyilvántartása');
 
-Scenario('ENYF Portál működése', (I) => {
+Scenario('Az EVNY Portál üzemel', (I) => {
     I.amOnPage('https://www.nyilvantarto.hu/evny-lekerdezo/');
     I.see('Egyéni vállalkozók nyilvántartása');
+});
+
+Scenario('A vállalkozás állapota működő', (I) => {
+    console.log('  * Nyilvántartási szám: ' + process.env.NYILVANTARTASI_SZAM);
+    I.amOnPage('https://www.nyilvantarto.hu/evny-lekerdezo/');
+    I.fillField('form[name=form2] input[name=nyilvszam]', process.env.NYILVANTARTASI_SZAM);
+    I.click('Lekérdezés', 'form[name=form2]');
+    I.wait(1);
+    I.see('Működő');
+    I.saveScreenshot('evny.png', true);
 });
