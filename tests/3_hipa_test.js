87dcf2b58291d50b6b1b13dba43b0696cb042ada
Balázs Kovács <kobalazs@gmail.com>
2019-03-17 18:13:17 +0100

    Implement: HIPA test (works, but needs performance improvement)

--- tests/3_hipa_test.js
+++ tests/3_hipa_test.js
@@ -6,29 +6,22 @@ Scenario('A Budapesti Adó Főosztály oldala üzemel', (I) => {
     I.waitForText('E-ügyintézés');
 });
 
-Scenario('Az Ügyfélkapu oldala üzemel', (I) => {
-    I.amOnPage('https://gate.gov.hu/sso/ap/ApServlet?partnerid=bpfph&target=https-ssl.budapest.hu-443-web_hair');
-    I.see('Lépjen be az Ügyfélkapun!');
-});
-
 Scenario('Az adószámla kivonat letöltve', (I) => {
     console.log('  ℹ Ügyfélkapu felhasználónév: ' + process.env.UGYFELKAPU_FELHASZNALONEV);
     console.log('  ℹ Ügyfélkapu jelszó: ' + '*'.repeat(process.env.UGYFELKAPU_JELSZO.length));
     console.log('  ✋ Az eredmény kézzel ellenőrizendő a results/hipa.png kép megtekintésével!');
 
-    I.amOnPage('https://ssl.budapest.hu/web_hair/mbej.do');
-    I.waitForText('Bejelentkezés ügyfélkapun');
-
-    I.click('Bejelentkezés');
-    I.waitForText('Lépjen be az Ügyfélkapun!');
+    I.amOnPage('https://gate.gov.hu/sso/ap/ApServlet?partnerid=bpfph&target=https-ssl.budapest.hu-443-web_hair');
+    I.wait(2);
     
     I.fillField('input[name=felhasznaloNev]', process.env.UGYFELKAPU_FELHASZNALONEV);
     I.fillField('input[name=jelszo]', process.env.UGYFELKAPU_JELSZO);
     I.click('Belépés');
+    I.wait(2);
     I.click('ide!');
-    I.waitForText('E-ügyintézés');
+    I.wait(2);
 
-    I.click('Adószámla lekérdezése');
+    I.amOnPage('https://ssl.budapest.hu/web_hair/fszla.do');
     I.waitForText('Adószámla kivonat');
     I.saveScreenshot('hipa.png', true);
 });
